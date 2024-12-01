80b45fe3424ae2c2747654c011ee8233390d56f0
ErionG14 <98408014+ErionG14@users.noreply.github.com>
2024-12-01 19:45:13 +0100

    deleted some mistakes from tailwind instalation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
+      <header className="text-center">
+        <img src={logo} className="mx-auto mb-6 w-32 h-32" alt="logo" />
+        <p className="mt-8 text-lg">
+          Edit <code className="font-mono text-yellow-300">src/App.js</code> and
+          save to reload.
         </p>
+        <p className="mt-4 text-lg">Edited by TailwindCSS!</p>
         <a
-          className="App-link"
+          className="mt-6 inline-block px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
